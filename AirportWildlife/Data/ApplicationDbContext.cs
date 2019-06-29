b7265bb97c6616f4218b22d81a9234971bf338aa
Kirren Covey <kirrencovey@gmail.com>
2019-06-29 18:08:53 -0500

    initial commit

---
+++ AirportWildlife/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Capstone.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirportWildlife.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            public DbSet<ApplicationUser> Users { get; set; }
+
+    }
+}
+}
---
+++ AirportWildlife/Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace AirportWildlife.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser() { }
+
+        [Required]
+        [Display(Name = "Account Name")]
+        public string AccountName { get; set; }
+
+        [Required]
+        [Display(Name = "Airport")]
+        public string Airport { get; set; }
+
+        public virtual ICollection<Interaction> Interactions { get; set; }
+    }
+}

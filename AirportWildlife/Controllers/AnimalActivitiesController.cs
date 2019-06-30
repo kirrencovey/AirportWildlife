c9cf968419f1492bd44b7744037adf5d31c7e3da
Kirren Covey <kirrencovey@gmail.com>
2019-06-30 12:13:49 -0500

    created controllers. animal activity controller and views are working

---
+++ AirportWildlife/Controllers/AnimalActivitiesController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AirportWildlife.Data;
+using AirportWildlife.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AirportWildlife.Controllers
+{
+    public class AnimalActivitiesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AnimalActivitiesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        // GET: AnimalActivities
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            var user = await GetCurrentUserAsync();
+            var applicationDbContext = _context.AnimalActivities.Include(a => a.User).Where(a => a.UserId == user.Id);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: AnimalActivities/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: AnimalActivities/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AnimalActivityId,Name")] AnimalActivity animalActivity)
+        {
+            var user = await GetCurrentUserAsync();
+
+            if (ModelState.IsValid)
+            {
+                animalActivity.User = user;
+                animalActivity.UserId = user.Id;
+                _context.Add(animalActivity);
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(animalActivity);
+        }
+
+        // GET: AnimalActivities/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var animalActivity = await _context.AnimalActivities.FindAsync(id);
+            if (animalActivity == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", animalActivity.UserId);
+            return View(animalActivity);
+        }
+
+        // POST: AnimalActivities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AnimalActivityId,Name,UserId")] AnimalActivity animalActivity)
+        {
+            if (id != animalActivity.AnimalActivityId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(animalActivity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AnimalActivityExists(animalActivity.AnimalActivityId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", animalActivity.UserId);
+            return View(animalActivity);
+        }
+
+        // GET: AnimalActivities/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var animalActivity = await _context.AnimalActivities
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(m => m.AnimalActivityId == id);
+            if (animalActivity == null)
+            {
+                return NotFound();
+            }
+
+            return View(animalActivity);
+        }
+
+        // POST: AnimalActivities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var animalActivity = await _context.AnimalActivities.FindAsync(id);
+            _context.AnimalActivities.Remove(animalActivity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AnimalActivityExists(int id)
+        {
+            return _context.AnimalActivities.Any(e => e.AnimalActivityId == id);
+        }
+    }
+}
---
+++ AirportWildlife/Controllers/InteractionsController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AirportWildlife.Data;
+using AirportWildlife.Models;
+
+namespace AirportWildlife.Controllers
+{
+    public class InteractionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InteractionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Interactions
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Interactions.Include(i => i.AnimalActivity).Include(i => i.ControlMethod).Include(i => i.Employee).Include(i => i.Habitat).Include(i => i.Species).Include(i => i.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Interactions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var interaction = await _context.Interactions
+                .Include(i => i.AnimalActivity)
+                .Include(i => i.ControlMethod)
+                .Include(i => i.Employee)
+                .Include(i => i.Habitat)
+                .Include(i => i.Species)
+                .Include(i => i.User)
+                .FirstOrDefaultAsync(m => m.InteractionId == id);
+            if (interaction == null)
+            {
+                return NotFound();
+            }
+
+            return View(interaction);
+        }
+
+        // GET: Interactions/Create
+        public IActionResult Create()
+        {
+            ViewData["AnimalActivityId"] = new SelectList(_context.AnimalActivities, "AnimalActivityId", "Name");
+            ViewData["ControlMethodId"] = new SelectList(_context.ControlMethods, "ControlMethodId", "Name");
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Initials");
+            ViewData["HabitatId"] = new SelectList(_context.Habitats, "HabitatId", "Name");
+            ViewData["SpeciesId"] = new SelectList(_context.Species, "SpeciesId", "Name");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Interactions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("InteractionId,Location,SpeciesCount,NumberTaken,DateTime,Comments,SpeciesId,AnimalActivityId,HabitatId,ControlMethodId,EmployeeId,UserId")] Interaction interaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(interaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AnimalActivityId"] = new SelectList(_context.AnimalActivities, "AnimalActivityId", "Name", interaction.AnimalActivityId);
+            ViewData["ControlMethodId"] = new SelectList(_context.ControlMethods, "ControlMethodId", "Name", interaction.ControlMethodId);
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Initials", interaction.EmployeeId);
+            ViewData["HabitatId"] = new SelectList(_context.Habitats, "HabitatId", "Name", interaction.HabitatId);
+            ViewData["SpeciesId"] = new SelectList(_context.Species, "SpeciesId", "Name", interaction.SpeciesId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", interaction.UserId);
+            return View(interaction);
+        }
+
+        // GET: Interactions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var interaction = await _context.Interactions.FindAsync(id);
+            if (interaction == null)
+            {
+                return NotFound();
+            }
+            ViewData["AnimalActivityId"] = new SelectList(_context.AnimalActivities, "AnimalActivityId", "Name", interaction.AnimalActivityId);
+            ViewData["ControlMethodId"] = new SelectList(_context.ControlMethods, "ControlMethodId", "Name", interaction.ControlMethodId);
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Initials", interaction.EmployeeId);
+            ViewData["HabitatId"] = new SelectList(_context.Habitats, "HabitatId", "Name", interaction.HabitatId);
+            ViewData["SpeciesId"] = new SelectList(_context.Species, "SpeciesId", "Name", interaction.SpeciesId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", interaction.UserId);
+            return View(interaction);
+        }
+
+        // POST: Interactions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("InteractionId,Location,SpeciesCount,NumberTaken,DateTime,Comments,SpeciesId,AnimalActivityId,HabitatId,ControlMethodId,EmployeeId,UserId")] Interaction interaction)
+        {
+            if (id != interaction.InteractionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(interaction);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InteractionExists(interaction.InteractionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AnimalActivityId"] = new SelectList(_context.AnimalActivities, "AnimalActivityId", "Name", interaction.AnimalActivityId);
+            ViewData["ControlMethodId"] = new SelectList(_context.ControlMethods, "ControlMethodId", "Name", interaction.ControlMethodId);
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "Initials", interaction.EmployeeId);
+            ViewData["HabitatId"] = new SelectList(_context.Habitats, "HabitatId", "Name", interaction.HabitatId);
+            ViewData["SpeciesId"] = new SelectList(_context.Species, "SpeciesId", "Name", interaction.SpeciesId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", interaction.UserId);
+            return View(interaction);
+        }
+
+        // GET: Interactions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var interaction = await _context.Interactions
+                .Include(i => i.AnimalActivity)
+                .Include(i => i.ControlMethod)
+                .Include(i => i.Employee)
+                .Include(i => i.Habitat)
+                .Include(i => i.Species)
+                .Include(i => i.User)
+                .FirstOrDefaultAsync(m => m.InteractionId == id);
+            if (interaction == null)
+            {
+                return NotFound();
+            }
+
+            return View(interaction);
+        }
+
+        // POST: Interactions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var interaction = await _context.Interactions.FindAsync(id);
+            _context.Interactions.Remove(interaction);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InteractionExists(int id)
+        {
+            return _context.Interactions.Any(e => e.InteractionId == id);
+        }
+    }
+}

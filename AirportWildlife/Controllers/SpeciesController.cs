8883d43c5dac869d077a9f1b771a0e41e4c0c835
Kirren Covey <kirrencovey@gmail.com>
2019-06-30 16:29:08 -0500

    species controller and views done

--- AirportWildlife/Controllers/SpeciesController.cs
+++ AirportWildlife/Controllers/SpeciesController.cs
@@ -7,45 +7,34 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AirportWildlife.Data;
 using AirportWildlife.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AirportWildlife.Controllers
 {
     public class SpeciesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public SpeciesController(ApplicationDbContext context)
+        public SpeciesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
 
         // GET: Species
+        [Authorize]
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Species.Include(s => s.User);
+            var user = await GetCurrentUserAsync();
+            var applicationDbContext = _context.Species.Include(s => s.User).Where(s => s.UserId == user.Id);
             return View(await applicationDbContext.ToListAsync());
         }
 
-        // GET: Species/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var species = await _context.Species
-                .Include(s => s.User)
-                .FirstOrDefaultAsync(m => m.SpeciesId == id);
-            if (species == null)
-            {
-                return NotFound();
-            }
-
-            return View(species);
-        }
-
         // GET: Species/Create
+        [Authorize]
         public IActionResult Create()
         {
             ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
@@ -57,10 +46,14 @@ namespace AirportWildlife.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("SpeciesId,Name,UserId")] Species species)
+        public async Task<IActionResult> Create([Bind("SpeciesId,Name")] Species species)
         {
+            var user = await GetCurrentUserAsync();
+
             if (ModelState.IsValid)
             {
+                species.User = user;
+                species.UserId = user.Id;
                 _context.Add(species);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -70,6 +63,7 @@ namespace AirportWildlife.Controllers
         }
 
         // GET: Species/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -123,6 +117,7 @@ namespace AirportWildlife.Controllers
         }
 
         // GET: Species/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -142,6 +137,7 @@ namespace AirportWildlife.Controllers
         }
 
         // POST: Species/Delete/5
+        [Authorize]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
